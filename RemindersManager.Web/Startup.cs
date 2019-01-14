b869d227aa2a27aba82b15dd9b8d6eb4f0537d95
Vitaliy Zubritsky <vitaliy.zubritsky@gmail.com>
2019-01-15 00:51:01 +0200

    update app startup

--- RemindersManager.Web/Startup.cs
+++ RemindersManager.Web/Startup.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using RemindersManager.Web.Data;
+using System.IO;
 
 namespace RemindersManager.Web
 {
@@ -44,34 +38,23 @@ namespace RemindersManager.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            // still fail run in release config
+            if (!env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.Use(async (context, next) => {
+                    await next();
+                    if (context.Response.StatusCode == 404 &&
+                       !Path.HasExtension(context.Request.Path.Value) &&
+                       !context.Request.Path.Value.StartsWith("/api/"))
+                    {
+                        context.Request.Path = "/index.html";
+                        await next();
+                    }
+                });
+                app.UseMvcWithDefaultRoute();
+                app.UseDefaultFiles();
+                app.UseStaticFiles();
             }
-
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                if (!env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "buildProd");
-                }
-            });
-
         }
     }
 }
