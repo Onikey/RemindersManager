7ae21c89fc8418324d50569b6fe017cb44c89b78
Vitaliy Zubritsky <vitaliy.zubritsky@gmail.com>
2019-01-14 08:15:23 +0200

    add init migration

---
+++ RemindersManager.Web/Migrations/20190114061053_init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RemindersManager.Web.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reminders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Subject = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true),
+                    RemindDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AuthorId = table.Column<Guid>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsCancelled = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reminders", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reminders");
+        }
+    }
+}
