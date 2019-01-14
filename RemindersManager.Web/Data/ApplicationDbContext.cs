f64da541c7fed909cf304a08795286484273f598
Vitaliy Zubritsky <vitaliy.zubritsky@gmail.com>
2019-01-14 08:06:18 +0200

    Init project

---
+++ RemindersManager.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RemindersManager.Web.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemindersManager.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Reminder>();
+                //.HasOne(x => x.Author)
+                //.WithMany()
+                //.HasForeignKey(x => x.AuthorId);
+            builder.Entity<Reminder>()
+                .Property(x => x.RemindDate)
+                .HasColumnType("datetime2");
+        }
+
+        public DbSet<Reminder> Reminders { get; set; }
+    }
+}
---
+++ RemindersManager.Web/Entities/Reminder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemindersManager.Web.Entities
+{
+    public class Reminder
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Subject { get; set; }
+
+        public string Notes { get; set; }
+
+        public DateTime RemindDate { get; set; }
+
+        public Guid AuthorId { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public bool IsCancelled { get; set; } = false;
+    }
+}
