b1115899d3632ec6f97fb1efd09f084fe54dac50
Vitaliy Zubritsky <vitaliy.zubritsky.external@kreios.lu>
2019-03-04 17:53:51 +0200

    reminder job in progress

--- RemindersManager.Web/Services/RemindersService.cs
+++ RemindersManager.Web/Services/RemindersService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using RemindersManager.Web.Data;
 using RemindersManager.Web.Entities;
 using System;
@@ -11,20 +12,42 @@ namespace RemindersManager.Web.Services
 	public class RemindersService : IRemindersService
 	{
 		private readonly ApplicationDbContext dbContext;
+		private readonly IBackgroundTaskQueue queue;
+		private readonly IServiceProvider services;
 
-		public RemindersService(ApplicationDbContext dbContext)
+		public RemindersService(ApplicationDbContext dbContext, IBackgroundTaskQueue queue, IServiceProvider services)
 		{
 			this.dbContext = dbContext;
+			this.queue = queue;
+			this.services = services;
 		}
 
 		public async Task<IEnumerable<Reminder>> GetAll(Guid authorId)
 		{
-			return await dbContext.Reminders.Where(x => !x.IsCancelled && x.AuthorId == authorId).ToListAsync();
+			return await dbContext.Reminders
+				.Where(x => !x.IsCancelled && x.AuthorId == authorId)
+				.Select(x => new Reminder
+				{
+					Id = x.Id,
+					Subject = x.Subject,
+					Notes = x.Notes,
+					RemindDate = x.RemindDate.ToLocalTime()
+				})
+				.ToListAsync();
 		}
 
 		public async Task<Reminder> GetById(Guid authorId, Guid reminderId)
 		{
-			return await dbContext.Reminders.FirstOrDefaultAsync(x => !x.IsCancelled && x.Id == reminderId && x.AuthorId == authorId);
+			return await dbContext.Reminders
+				.Where(x => !x.IsCancelled && x.Id == reminderId && x.AuthorId == authorId)
+				.Select(x => new Reminder
+				{
+					Id = x.Id,
+					Subject = x.Subject,
+					Notes = x.Notes,
+					RemindDate = x.RemindDate.ToLocalTime()
+				})
+				.FirstOrDefaultAsync();
 		}
 
 		public async Task<bool> Create(Guid authorId, string subject, string notes, DateTime date)
@@ -34,11 +57,13 @@ namespace RemindersManager.Web.Services
 				AuthorId = authorId,
 				Subject = subject,
 				Notes = notes,
-				RemindDate = date
+				RemindDate = date.ToUniversalTime()
 			};
 
 			dbContext.Reminders.Add(reminder);
 			await dbContext.SaveChangesAsync();
+
+			AddRemindToQueue(reminder);
 
 			return true;
 		}
@@ -52,9 +77,9 @@ namespace RemindersManager.Web.Services
 				return false;
 			}
 
-			reminder.Notes = notes;
-			reminder.RemindDate = date;
 			reminder.Subject = subject;
+			reminder.Notes = notes;
+			reminder.RemindDate = date.ToUniversalTime();
 
 			await dbContext.SaveChangesAsync();
 
@@ -107,6 +132,45 @@ namespace RemindersManager.Web.Services
 			await dbContext.SaveChangesAsync();
 
 			return true;
+		}
+
+		private void AddRemindToQueue(Reminder reminder)
+		{
+			//no need to send remind
+			if (!reminder.IsActive || reminder.IsCancelled)
+			{
+				return;
+			}
+
+			queue.QueueBackgroundWorkItem(async token =>
+			{
+				var timeout = reminder.RemindDate - DateTime.UtcNow;
+
+				if (timeout.TotalSeconds > 0)
+				{
+					await Task.Delay(timeout, token);
+				}
+
+				using (var scope = services.CreateScope())
+				{
+					var remindersService =
+						scope.ServiceProvider
+							.GetRequiredService<RemindersService>();
+
+					//get actual data
+					reminder = await remindersService.GetById(reminder.AuthorId, reminder.Id);
+
+					//no need to send remind
+					if (!reminder.IsActive || reminder.IsCancelled)
+					{
+						return;
+					}
+
+					// todo send notification
+
+					await remindersService.Deactivate(reminder.AuthorId, reminder.Id);
+				}
+			});
 		}
 	}
 }
