ca1c1c7617001c2185fb83c3ac7b4b81635c69f3
Vitaliy Zubritsky <vitaliy.zubritsky.external@kreios.lu>
2019-03-04 17:53:31 +0200

    add auth server

--- RemindersManager.Web/Startup.cs
+++ RemindersManager.Web/Startup.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using RemindersManager.Web.Data;
 using RemindersManager.Web.Services;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace RemindersManager.Web
 {
@@ -25,22 +33,60 @@ namespace RemindersManager.Web
 			services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			services.AddIdentity<IdentityUser, IdentityRole>()
+					.AddEntityFrameworkStores<ApplicationDbContext>()
+					.AddDefaultTokenProviders();
+
+			JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
+			services
+				.AddAuthentication(options =>
+				{
+					options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+					options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+					options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+				})
+				.AddJwtBearer(cfg =>
+				{
+					cfg.RequireHttpsMetadata = false;
+					cfg.SaveToken = true;
+					cfg.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidIssuer = Configuration["JwtIssuer"],
+						ValidAudience = Configuration["JwtIssuer"],
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+						ClockSkew = TimeSpan.Zero // remove delay of token when expire
+					};
+				});
+
+
 
 			services.AddSpaStaticFiles(configuration =>
 			{
 				configuration.RootPath = "ClientApp/dist";
 			});
 
+			services.AddHostedService<QueuedHostedService>();
+			services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
 			services.AddScoped<IRemindersService, RemindersService>();
+
+			services.AddMvc(config =>
+			{
+				// using Microsoft.AspNetCore.Mvc.Authorization;
+				// using Microsoft.AspNetCore.Authorization;
+				var policy = new AuthorizationPolicyBuilder()
+								 .RequireAuthenticatedUser()
+								 .Build();
+				config.Filters.Add(new AuthorizeFilter(policy));
+			}).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext dbContext)
 		{
 			if (env.IsDevelopment())
 			{
-				UpdateDatabase(app);
+				dbContext.Database.Migrate();
 
 				app.UseDeveloperExceptionPage();
 			}
@@ -62,30 +108,16 @@ namespace RemindersManager.Web
 
 			app.UseSpa(spa =>
 			{
-					// To learn more about options for serving an Angular SPA from ASP.NET Core,
-					// see https://go.microsoft.com/fwlink/?linkid=864501
+				// To learn more about options for serving an Angular SPA from ASP.NET Core,
+				// see https://go.microsoft.com/fwlink/?linkid=864501
 
-					spa.Options.SourcePath = "ClientApp";
+				spa.Options.SourcePath = "ClientApp";
 
 				if (env.IsDevelopment())
 				{
 					spa.UseAngularCliServer(npmScript: "start");
 				}
 			});
-		}
-
-		private static void UpdateDatabase(IApplicationBuilder app)
-		{
-
-			using (var serviceScope = app.ApplicationServices
-				.GetRequiredService<IServiceScopeFactory>()
-				.CreateScope())
-			{
-				using (var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>())
-				{
-					context.Database.Migrate();
-				}
-			}
 		}
 	}
 }
